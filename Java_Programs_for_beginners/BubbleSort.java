ffc526f8ad3bec5c12473d65fcbc3f699e5726b5
priyanka-0207 <80313973+priyanka-0207@users.noreply.github.com>
2023-10-15 02:36:45 +0530

    Update BubbleSort.java

--- Java_Programs_for_beginners/BubbleSort.java
+++ Java_Programs_for_beginners/BubbleSort.java
@@ -1,9 +1,10 @@
+import java.util.Scanner;
 class BubbleSort { 
 	void bubbleSort(int arr[]) 
 	{ 
-		int n = arr.length; 
-		for (int i = 0; i < n - 1; i++) 
-			for (int j = 0; j < n - i - 1; j++) 
+		int len = arr.length; 
+		for (int i = 0; i < len - 1; i++) 
+			for (int j = 0; j < len - i - 1; j++) 
 				if (arr[j] > arr[j + 1]) { 
 					// swap temp and arr[i] 
 					int temp = arr[j]; 
@@ -14,8 +15,8 @@ class BubbleSort {
 
 	void printArray(int arr[]) 
 	{ 
-		int n = arr.length; 
-		for (int i = 0; i < n; ++i) 
+		int len = arr.length; 
+		for (int i = 0; i < len; ++i) 
 			System.out.print(arr[i] + " "); 
 		System.out.println(); 
 	} 
@@ -23,7 +24,7 @@ class BubbleSort {
 	public static void main(String args[]) 
 	{ 
 		BubbleSort ob = new BubbleSort(); 
-		int arr[] = { 64, 34, 25, 12, 22, 11, 90 }; 
+		int arr[] = { 78, 23, 29, 17, 25, 14, 98 }; 
 		ob.bubbleSort(arr); 
 		System.out.println("Sorted array"); 
 		ob.printArray(arr); 
